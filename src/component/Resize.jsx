f34477a7f396171c008bfb7a39bc7486fcbba312
Nazmul Hasan <nazmulprime162001@gmail.com>
2023-03-16 14:50:58 +0600

    update boxGeometry

--- src/component/Resize.jsx
+++ src/component/Resize.jsx
@@ -13,9 +13,29 @@ const Resize = () => {
     // Scene
     const scene = new THREE.Scene()
 
+    // PositionArray32
+    // const positionArray = new Float32Array(9)
+    // positionArray[0] = 0
+    // positionArray[1] = 0
+    // positionArray[2] = 0
+
+    // positionArray[3] = 0
+    // positionArray[4] = 1
+    // positionArray[5] = 0
+
+    // positionArray[6] = 1
+    // positionArray[7] = 0
+    // positionArray[8] = 0
+    
+    const geometry2 = new THREE.BufferGeometry()
+    
+
     // Object
-    const geometry = new THREE.BoxGeometry(1, 1, 1)
-    const material = new THREE.MeshBasicMaterial({ color: 0xff0000 })
+    const geometry = new THREE.BoxGeometry(1, 1, 1, 2, 2, 2,)
+    const material = new THREE.MeshBasicMaterial({ 
+      color: 0xff0000,
+      wireframe: true
+    })
     const mesh = new THREE.Mesh(geometry, material)
     scene.add(mesh)
 
